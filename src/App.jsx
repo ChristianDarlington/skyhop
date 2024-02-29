97f9a19ce296f41671c04350c5c3f17d82e6f3d8
Christian <christiandarlington0@gmail.com>
2024-02-29 09:35:38 -0500

    folders and files are setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Components/Header';
+import Import from './Components/Import';
+import RightSide from './Components/RightSide';
+import Upload from './Components/Upload';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Import />
+      <RightSide />
+      <Upload />
+    </div>
+  );
+}
+
+export default App;
